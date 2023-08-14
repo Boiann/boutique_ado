5a2702f6b0a462607661501d7ccb30137e6f4c2e
Bojan Bakaric <bakaricbojan@gmail.com>
2023-08-14 07:11:23 +0000

    Add bootstrap toasts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--tghg9v6-7yp#mq&0n@rms^gv$_qpixhhnj1=!5gul3hge0x27
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-boiann-boutiqueado-gun9ie7y98u.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-boiann-boutiqueado-gun9ie7y98u.ws-eu103.gitpod.io']
 
 
 # Application definition
@@ -78,6 +78,8 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
